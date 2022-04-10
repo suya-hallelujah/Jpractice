2547256876f74d889e2176db8fc7ebbab6f4622e
Suya <suyakozh@gmail.com>
2022-04-10 11:04:59 -0500

    Stacks

---
+++ Stacks/src/main/java/CollectionsStack.java
@@ -0,0 +1,19 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class CollectionsStack {
+    public static void main(String[] args) {
+
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < 5;i++) {
+            stack.push(i);
+        }
+        System.out.println("Stack elements are:");
+        stack.forEach(System.out::println);
+        System.out.println("Top of the stack is:   "+ stack.peek());
+        stack.pop();
+        stack.pop();
+        System.out.println("Updated Stack:   ");
+        stack.forEach(System.out::println);
+    }
+}
